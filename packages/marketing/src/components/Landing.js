ed4a48d61530d4eddbd992226e0f5e43be639aa3
johnny0221 <jim850221@gmail.com>
2022-09-04 21:22:42 +0800

    feat: update marketing

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -78,7 +78,7 @@ export default function Album() {
               align='center'
               color='textPrimary'
               gutterBottom>
-              Home Page
+              Home Pages
             </Typography>
             <Typography
               variant='h5'
